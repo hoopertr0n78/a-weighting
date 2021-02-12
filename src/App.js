289fe480d21059c78e9b5b9c81ec085614df74c9
Chris <hello@chrishooper.io>
2021-02-12 15:19:13 +0000

    Add a table (handsontable)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HotTable } from '@handsontable/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+      ['2016', 10, 11, 12, 13],
+      ['2017', 20, 11, 14, 13],
+      ['2018', 30, 15, 12, 13],
+    ];
+  }
+
+  render() {
+    return (
+      <div id="hot-app">
+        <HotTable
+          data={this.data}
+          colHeaders={true}
+          rowHeaders={true}
+          width="600"
+          height="300"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;

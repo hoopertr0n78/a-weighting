6c72a0b2d4161b5b3325c18561f7e9476ee3f0e6
Chris <hello@chrishooper.io>
2021-02-12 15:56:05 +0000

    Add a simple A-weighting table

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.data = [
-      ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
-      ['2016', 10, 11, 12, 13],
-      ['2017', 20, 11, 14, 13],
-      ['2018', 30, 15, 12, 13],
+      ['100', '100', '100', '100', '100', '100', '100', '100'],
+      [-26.2, -16.1, -8.6, -3.2, 0, 1.2, 1, -1.1],
+      ['=A1+A2', '=B1+B2', '=C1+C2', '=D1+D2', '=E1+E2', '=F1+F2', '=G1+G2', '=H1+H2'],
     ];
   }
 
@@ -17,10 +16,12 @@ class App extends React.Component {
       <div id="hot-app">
         <HotTable
           data={this.data}
-          colHeaders={true}
-          rowHeaders={true}
+          colHeaders={['63', '125', '250', '500', '1k', '2k', '4k', '8k']}
+          rowHeaders={['Input', 'A', 'Output']}
+          formulas={[true]}
           width="600"
           height="300"
+          licenseKey="non-commercial-and-evaluation"
         />
       </div>
     );
